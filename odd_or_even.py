12b28649fc62409ba886b114ece433d333479b5f
Sairam-Nagarajan <139829666+atcodersairam@users.noreply.github.com>
2024-06-26 19:20:45 +0530

    Create odd_or_even.py

---
+++ odd_or_even.py
@@ -0,0 +1,10 @@
+Problem_link = "https://www.geeksforgeeks.org/problems/odd-or-even3618/1"
+
+#User function Template for python3
+class Solution:
+    def oddEven (ob,N):
+        # code here 
+        if N%2==0:
+            return "even"
+        else:
+            return "odd"
